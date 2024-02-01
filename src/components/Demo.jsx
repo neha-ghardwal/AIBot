bad60654e7841df71793fb5a9f7767c3a9646c27
neha-ghardwal <ghardwalneha15@gmail.com>
2024-02-02 02:04:59 +0530

    API integrated

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,7 +9,20 @@ const Demo = () => {
     summary:'',
   });
 
+  const [allarticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState([]);
+
   const [getSummary , {error, isFetching}] = useLazyGetSummaryQuery();
+//passing the data as string
+  useEffect(() => {
+    const articlesLocal = JSON.parse(
+      localStorage.getItem('articles')
+    )
+    if(articlesLocal){
+      setAllArticles(articlesLocal)
+
+    }
+  },[]);
 
   const handleSubmit = async(e) =>{
     e.preventDefault();
@@ -17,10 +30,21 @@ const Demo = () => {
 
     if(data?.summary){
       const newArticle = {...article, summary: data.summary };
+      //showing history
+      const updatedArticles = [newArticle, ...allarticles];
       setArticle(newArticle);
+      setAllArticles(updatedArticles);
+//stringify cz  localstorage only save strings
+      localStorage.setItem('articles',JSON.stringify(updatedArticles))
       
-      console.log(newArticle);
+      // console.log(newArticle);
     }
+  }
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false),3000);
   }
 
 
@@ -54,8 +78,48 @@ const Demo = () => {
         </button>
         </form>
         {/* history */}
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+          {allarticles.map((item,index) =>(
+            <div key={`link-${index}`}
+            onClick={() => setArticle(item)}
+            className='link_card'
+            >
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img src={copied === item.url ? tick : copy} alt="copyimg" className="w-[40%] h-[40%] object-contain"/>
+              </div>
+              <p className="flex-1 font-inter text-blue-500 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       {/* display results */}
+      <div className="my-10 max-w-full flex justify-between items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="h-20 w-200 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Oops, Something went wrong
+            <br />
+            <span className="font-inter font-normal text-gray-600">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box"> 
+                <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
+
+      </div>
     </section>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,9 @@
-// import { logo }  from '../assets'
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-        <nav className="flex justify-between items-center w-full m-10 pt-3">
-            {/* <img src={logo2} alt='ai_logo' className='w-28 object-contain' /> */}
-            <button 
-                type='button'
-                onClick={() => window.open('https://github.com')}
-                className='black_btn'
-            >
-                Home
-            </button>
-            <button
-                type='button'
-                onClick={() => window.open('https://github.com')}
-                className='black_btn'
-            >
-                Github
-            </button>
-        </nav>
+        
         <h1 className='head_text'>
-            Summarize text with <br className='max-md:hidden'/>
+            Snip your Article with <br className='max-md:hidden'/>
             <span className='orange_gradient'>open AI GPT-4</span>
         </h1>
         <h2 className="desc">
